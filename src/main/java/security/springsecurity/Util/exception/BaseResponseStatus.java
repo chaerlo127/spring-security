dbfda10f87017934dc2d60c310662d71f0a3678c
chaerlo127 <monynony0203@gmail.com>
2023-02-27 01:56:09 +0900

    feat: token exception 추가

--- src/main/java/security/springsecurity/Util/exception/BaseResponseStatus.java
+++ src/main/java/security/springsecurity/Util/exception/BaseResponseStatus.java
@@ -4,7 +4,18 @@ import lombok.Getter;
 
 @Getter
 public enum BaseResponseStatus {
-    SUCCESS(true, 1000, "요청에 성공하였습니다.");
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+    /*
+     * 9500 : jwt
+     * */
+    WRONG_JWT_SIGN_TOKEN(false, 9500, "잘못된 JWT 서명입니다."),
+    EXPIRED_JWT_TOKEN(false, 9501, "만료된 JWT 토큰 입니다."),
+    UNSUPPORTED_JWT_TOKEN(false, 9502, "지원되지 않는 JWT 토큰입니다."),
+    WRONG_JWT_TOKEN(false, 9503, "JWT 토큰이 잘못되었습니다."),
+    NULL_JWT(false,9504, "JWT의 값이 없습니다."),
+    INVALID_JWT_TOKEN(false, 9505, "Refresh Token 이 유효하지 않습니다."),
+    NOT_SAME_USER_INFO(false, 9506, "토큰의 유저 정보가 일치하지 않습니다."),
+    LOGOUT_USER(false, 9507, "로그아웃된 사용자입니다.");
     private final boolean isSuccess;
     private final int code;
     private final String message;

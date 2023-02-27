d1d26c367c7a6cf2bde6937423cff69d47cf65ac
chaerlo127 <monynony0203@gmail.com>
2023-02-27 16:03:14 +0900

    feat: Dao 생성

---
+++ src/main/java/security/springsecurity/user/DAO/UserDao.java
@@ -0,0 +1,11 @@
+package security.springsecurity.user.DAO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserDao {
+    private Long userIdx;
+    private String userId;
+}

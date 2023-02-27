76f75ebf0aeafee4539e6f216f59407f0d51054e
chaerlo127 <monynony0203@gmail.com>
2023-02-27 14:00:29 +0900

    feat: JwtSecurityConfig 생성

---
+++ src/main/java/security/springsecurity/Util/JwtSecurityConfig.java
@@ -0,0 +1,26 @@
+package security.springsecurity.Util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import security.springsecurity.user.jwt.JwtFilter;
+import security.springsecurity.user.jwt.TokenProvider;
+
+@RequiredArgsConstructor
+@Configuration
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        JwtFilter jwtFilter = new JwtFilter(tokenProvider);
+        /**
+         * UsernamePasswordAuthenticationFilter: 설정된 로그인 URL로 오는 요청을 감시하여 유저 인증 처리
+         */
+        // security filter가 실행되기 전에 jwtFilter가 먼저 실행되도록 jwtfilter를 SecurityFilter 앞에 추가
+        builder.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
